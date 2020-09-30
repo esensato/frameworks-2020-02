608e931b384fc45a73455b44f647f98c56694f89
Edson A. Sensato <esensato@hotmail.com>
2020-09-29 22:29:41 -0300

    Aula 29/09

--- ant-design/src/index.js
+++ ant-design/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import { useState } from 'react'
 
+import axios from 'axios';
+
 import { ClockCircleOutlined, Html5Outlined,  CheckSquareOutlined, DownOutlined, CaretUpOutlined, CaretDownOutlined} from '@ant-design/icons';
-import {PageHeader, Button, Input, Space, List, Dropdown, Menu, Checkbox, Radio, DatePicker, Progress} from 'antd'
+import {PageHeader, Button, Input, Space, List, Dropdown, Menu, Checkbox, Radio, DatePicker, Progress, Slider, Card, Row, Col, Timeline} from 'antd'
 
 function TesteIcone() {
     
@@ -22,6 +24,12 @@ function TesteHeader() {
     const [despertador, setDespertador] = useState(1)
 
     const [percentual, setPercentual] = useState(10)
+
+    const [nota, setNota] = useState(5)
+
+    const [atividadeDigitada, setAtividadeDigitada] = useState("")
+
+    const [atividades, setAtividades] = useState([])
 
     const routes = [ {path: 'nivel_um', breadcrumbName: 'Menu'},
     {path: 'nivel_dois', breadcrumbName: 'Cadastro' },
@@ -77,6 +85,41 @@ function TesteHeader() {
         setPercentual(percentual - 10)
     }
 
+    const adicionarAtividade = () => {
+        let tmp = [...atividades]
+        tmp.push(atividadeDigitada)
+        setAtividades(tmp)
+        console.log(tmp)
+
+    }
+
+    // obtem a lista de trabalhos cadastrados
+    const obterTrabalhos = () => {
+
+        axios.get("https://trabalho-react.glitch.me/trabalho").then((resposta) => {
+            console.log(resposta.data)
+            let tmp = [...atividades]
+            for (let i = 0; i < resposta.data.length; i++) {
+                tmp.push(resposta.data[i].titulo)
+            }
+            setAtividades(tmp)
+            console.log(tmp)
+        })
+
+    }
+
+    // cria um novo usuario para o aluno
+    const criarUsuario = () => {
+        const dados = {username: nome}
+        axios.post("https://trabalho-react.glitch.me/aluno", dados).then((res) => {
+            if (res.status == 200) {
+             console.log(res.data)
+            }
+            }).catch ((err) => {
+             console.log('ERRO: ', err);
+            })
+    }
+
 return <>
             <PageHeader title="Cadastro"
             subTitle="Cadastrar notas"
@@ -92,6 +135,10 @@ return <>
                 <Space>
                     <Button type="primary" onClick={botaoClick} icon={<CheckSquareOutlined />}>Mensagem</Button>
                     <Button type="primary" onClick={adicionar} icon={<ClockCircleOutlined />}>Adicionar</Button>
+
+                    <Button type="primary" onClick={criarUsuario} icon={<ClockCircleOutlined />}>Criar Usuario</Button>
+
+
                     <Checkbox onChange={ativar} checked={ativo} disabled={despertador == 0 ? false : true}>Ativar planejamento</Checkbox>
                     <Radio.Group value={despertador} onChange={acordar}>
                         <Radio key="0" value={0}>Acordar</Radio>
@@ -112,8 +159,58 @@ return <>
                     <Button onClick={aumenta}><CaretUpOutlined /></Button>
                     <Button onClick={diminui}><CaretDownOutlined /></Button>
                 </Button.Group>
-                
+
+                <Slider min={0} max={10} value={nota} onChange={(val) => setNota(val)}/>
+                <Button disabled={nota < 6 ? true : false}>Enviar nota {nota}</Button>
+
+                <Card color="green" bordered={true} title="Atividade Critica">Terminar a refatoração do código Java</Card>
+
+                <Timeline>
+
+                      <Timeline.Item color="green">Início</Timeline.Item>
+                      <Timeline.Item>Marginal Pinheiros</Timeline.Item>
+                      <Timeline.Item dot={<ClockCircleOutlined />}>Butantã</Timeline.Item>
+                      <Timeline.Item color="red">Fim</Timeline.Item>
+
+                </Timeline>
+
             </Space>
+
+            <div>
+                <Row gutter={[5, 5]}>
+                    <Col><Button>Botao 1</Button></Col>
+                    <Col span={2}><Button>Botao 2</Button></Col>
+                </Row>
+
+                <Row gutter={[5, 5]}>
+                    <Col><Button>Botao 1</Button></Col>
+                    <Col><Button>Botao 2</Button></Col>
+                    <Col><Button>Botao 3</Button></Col>
+                </Row>
+
+                <Row gutter={[5, 5]}>
+                    <Col span={20} style={{border: "solid"}}><Button>Botao 4</Button></Col>
+                    <Col span={4}><Button>Botao 4</Button></Col>
+                </Row>
+
+            </div>
+
+            <Space direction="vertical">
+
+                <Input addonBefore="Tarefa:" placeholder="Informe a tarefa" 
+                       onChange={(e) => setAtividadeDigitada(e.target.value)} 
+                       value={atividadeDigitada}/>
+                <Button type="primary" onClick={adicionarAtividade}>Adicionar</Button>
+                <Timeline>
+
+                    {atividades.map((item, idx) => <Timeline.Item color={idx % 2 == 0 ? "green" : "red"} key={idx}>{item}</Timeline.Item> )}
+
+                </Timeline>
+
+            </Space>
+
+            <Button type="primary" onClick={obterTrabalhos}>Obter trabalhos</Button>
+
             </>
 
 }
